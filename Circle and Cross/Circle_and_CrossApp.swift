d125492c8385a8cc521afa1803387221ef351ac8
Łukasz <Solik>
2022-11-19 15:27:39 +0100

    Initial Commit

---
+++ Circle and Cross/Circle_and_CrossApp.swift
@@ -0,0 +1,17 @@
+//
+//  Circle_and_CrossApp.swift
+//  Circle and Cross
+//
+//  Created by Łukasz Solik on 19/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Circle_and_CrossApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Circle and CrossUITests/Circle_and_CrossUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Circle_and_CrossUITestsLaunchTests.swift
+//  Circle and CrossUITests
+//
+//  Created by Łukasz Solik on 19/11/2022.
+//
+
+import XCTest
+
+final class Circle_and_CrossUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
